2364e77027cf58cfe03ad07db39afb9698ee90f7
Robin Link <MailRobinLink@gmail.com>
2018-01-03 17:26:02 +0100

    Started GUI development on new branch (gui) and created hello world window

--- src/main/java/edu/kit/ipd/dbis/Main.java
+++ src/main/java/edu/kit/ipd/dbis/Main.java
@@ -1,5 +1,8 @@
 package edu.kit.ipd.dbis;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  * Grape's main class.
  */
@@ -10,6 +13,13 @@ public class Main {
      * @param args arguments provided when run using the command line.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame frame = new JFrame("Hello World!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Hello World!");
+        frame.getContentPane().add(label);
+
+        frame.pack();
+        frame.setVisible(true);
     }
 }
