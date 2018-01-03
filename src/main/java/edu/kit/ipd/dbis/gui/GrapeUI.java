f3ff509d55afa75de89466efb7a9cdced0e16aa6
Robin Link <MailRobinLink@gmail.com>
2018-01-03 21:40:15 +0100

    Moved makeMenuBar to MenuUI.java

--- src/main/java/edu/kit/ipd/dbis/gui/GrapeUI.java
+++ src/main/java/edu/kit/ipd/dbis/gui/GrapeUI.java
@@ -4,7 +4,8 @@
 
 package edu.kit.ipd.dbis.gui;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import java.util.ResourceBundle;
 
 public class GrapeUI extends JPanel {
@@ -21,53 +22,9 @@ public class GrapeUI extends JPanel {
 		JFrame mainWindow = new JFrame(programName);
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		mainWindow.setJMenuBar(makeMenuBar());
+		mainWindow.setJMenuBar(MenuUI.makeMenuBar(language));
 
 		mainWindow.pack();
 		mainWindow.setVisible(true);
 	}
-	
-	private JMenuBar makeMenuBar() {
-		JMenuBar menuBar = new JMenuBar();
-
-		JMenu file = new JMenu(language.getString("file"));
-		JMenuItem newDatabase = new JMenuItem(language.getString("newDatabase"));
-		JMenuItem openDatabase = new JMenuItem(language.getString("openDatabase"));
-		JMenuItem saveDatabase = new JMenuItem(language.getString("saveDatabase"));
-		JMenuItem saveSelection = new JMenuItem(language.getString("saveSelection"));
-		JMenuItem importDatabase = new JMenuItem(language.getString("importDatabase"));
-		JMenuItem saveDatabaseAs = new JMenuItem(language.getString("saveDatabaseAs"));
-		file.add(newDatabase);
-		file.add(openDatabase);
-		file.add(importDatabase);
-		file.add(saveDatabase);
-		file.add(saveDatabaseAs);
-		file.add(saveSelection);
-		menuBar.add(file);
-
-		JMenu edit = new JMenu(language.getString("edit"));
-		JMenuItem generateGraphs = new JMenuItem(language.getString("generateGraph"));
-		JMenuItem emptyGraph = new JMenuItem(language.getString("emptyGraph"));
-		JMenuItem readBFSCode = new JMenuItem(language.getString("readBFSCode"));
-		JMenuItem Undo = new JMenuItem(language.getString("undo"));
-		JMenuItem Redo = new JMenuItem(language.getString("redo"));
-		edit.add(generateGraphs);
-		edit.add(emptyGraph);
-		edit.add(readBFSCode);
-		edit.addSeparator();
-		edit.add(Undo);
-		edit.add(Redo);
-		menuBar.add(edit);
-
-		JMenu help = new JMenu(language.getString("help"));
-		JMenuItem info = new JMenuItem(language.getString("info"));
-		JMenuItem documentation = new JMenuItem(language.getString("documentation"));
-		help.add(info);
-		help.add(documentation);
-		menuBar.add(help);
-
-		return menuBar;
-	}
-
-
 }
--- src/main/java/edu/kit/ipd/dbis/gui/MenuUI.java
+++ src/main/java/edu/kit/ipd/dbis/gui/MenuUI.java
@@ -4,5 +4,51 @@
 
 package edu.kit.ipd.dbis.gui;
 
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import java.util.ResourceBundle;
+
 public class MenuUI {
+	public static JMenuBar makeMenuBar(ResourceBundle language) {
+		JMenuBar menuBar = new JMenuBar();
+
+		JMenu file = new JMenu(language.getString("file"));
+		JMenuItem newDatabase = new JMenuItem(language.getString("newDatabase"));
+		JMenuItem openDatabase = new JMenuItem(language.getString("openDatabase"));
+		JMenuItem saveDatabase = new JMenuItem(language.getString("saveDatabase"));
+		JMenuItem saveSelection = new JMenuItem(language.getString("saveSelection"));
+		JMenuItem importDatabase = new JMenuItem(language.getString("importDatabase"));
+		JMenuItem saveDatabaseAs = new JMenuItem(language.getString("saveDatabaseAs"));
+		file.add(newDatabase);
+		file.add(openDatabase);
+		file.add(importDatabase);
+		file.add(saveDatabase);
+		file.add(saveDatabaseAs);
+		file.add(saveSelection);
+		menuBar.add(file);
+
+		JMenu edit = new JMenu(language.getString("edit"));
+		JMenuItem generateGraphs = new JMenuItem(language.getString("generateGraph"));
+		JMenuItem emptyGraph = new JMenuItem(language.getString("emptyGraph"));
+		JMenuItem readBFSCode = new JMenuItem(language.getString("readBFSCode"));
+		JMenuItem Undo = new JMenuItem(language.getString("undo"));
+		JMenuItem Redo = new JMenuItem(language.getString("redo"));
+		edit.add(generateGraphs);
+		edit.add(emptyGraph);
+		edit.add(readBFSCode);
+		edit.addSeparator();
+		edit.add(Undo);
+		edit.add(Redo);
+		menuBar.add(edit);
+
+		JMenu help = new JMenu(language.getString("help"));
+		JMenuItem info = new JMenuItem(language.getString("info"));
+		JMenuItem documentation = new JMenuItem(language.getString("documentation"));
+		help.add(info);
+		help.add(documentation);
+		menuBar.add(help);
+
+		return menuBar;
+	}
 }
