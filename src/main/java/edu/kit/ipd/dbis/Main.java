800809aea04e5c4bb226c2f9406c9ea8460ea208
Robin Link <MailRobinLink@gmail.com>
2018-01-03 16:58:34 +0100

    Added comments to the Main class

--- src/main/java/edu/kit/ipd/dbis/Main.java
+++ src/main/java/edu/kit/ipd/dbis/Main.java
@@ -1,6 +1,14 @@
 package edu.kit.ipd.dbis;
 
+/**
+ * Grape's main class.
+ */
 public class Main {
+
+    /**
+     * Grape's main method.
+     * @param args arguments provided when run using the command line.
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
