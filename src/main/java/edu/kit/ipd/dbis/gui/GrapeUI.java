43bd59741ab4eace7f4f8bdfc76de82a500e687e
Robin Link <MailRobinLink@gmail.com>
2018-01-04 00:17:41 +0100

    Adjusted layout of the JPanels

--- src/main/java/edu/kit/ipd/dbis/gui/GrapeUI.java
+++ src/main/java/edu/kit/ipd/dbis/gui/GrapeUI.java
@@ -9,11 +9,11 @@ import edu.kit.ipd.dbis.gui.theme.LightTheme;
 import edu.kit.ipd.dbis.gui.theme.Theme;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ResourceBundle;
 
 public class GrapeUI extends JFrame {
 
-	private final String programName = "Grape";
 	private ResourceBundle language;
 	private Theme theme = new LightTheme();
 
@@ -23,11 +23,15 @@ public class GrapeUI extends JFrame {
 	}
 
 	private void run() {
+		String programName = "Grape";
 		JFrame mainWindow = new JFrame(programName);
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.setMinimumSize(new Dimension(600, 400));
+		mainWindow.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+		mainWindow.setJMenuBar(MenuUI.makeMenuBar(language, theme));
 
 		JPanel filterUI = FilterUI.makeFilterUI(language, theme);
-
 		JPanel correlationUI = CorrelationUI.makeCorrelationUI(language, theme);
 
 		JSplitPane filterCorrelationDivider = new JSplitPane(
@@ -36,6 +40,7 @@ public class GrapeUI extends JFrame {
 				filterUI,
 				correlationUI);
 		filterCorrelationDivider.setDividerSize(theme.dividerSize);
+		filterCorrelationDivider.setBorder(null);
 
 		JPanel graphEditor = GraphEditorUI.makeGraphEditorUI(language, theme);
 
@@ -45,6 +50,7 @@ public class GrapeUI extends JFrame {
 				filterCorrelationDivider,
 				graphEditor);
 		graphEditorDivider.setDividerSize(theme.dividerSize);
+		graphEditorDivider.setBorder(null);
 
 		JPanel leftUI = new JPanel();
 		leftUI.setLayout(new BoxLayout(leftUI, BoxLayout.Y_AXIS));
@@ -52,15 +58,16 @@ public class GrapeUI extends JFrame {
 		leftUI.setForeground(theme.foregroundColor);
 		leftUI.add(graphEditorDivider);
 
+		graphEditorDivider.setResizeWeight(.55f);
+		filterCorrelationDivider.setResizeWeight(.5f);
+
 		JPanel tableUI = TableUI.makeTableUI(language, theme);
 		JPanel statusbarUI = StatusbarUI.makeStatusbarUI(language, theme);
 		JPanel rightUI = new JPanel();
 
 		rightUI.setLayout(new BoxLayout(rightUI, BoxLayout.Y_AXIS));
-		rightUI.add(tableUI);
-		rightUI.add(statusbarUI);
-		rightUI.setBackground(theme.backgroundColor);
-		rightUI.setForeground(theme.foregroundColor);
+		rightUI.add(tableUI, BorderLayout.NORTH);
+		rightUI.add(statusbarUI, BorderLayout.SOUTH);
 
 		JSplitPane verticalDivider = new JSplitPane(
 				JSplitPane.HORIZONTAL_SPLIT,
@@ -68,12 +75,10 @@ public class GrapeUI extends JFrame {
 				leftUI,
 				rightUI);
 		verticalDivider.setDividerSize(theme.dividerSize);
+		verticalDivider.setResizeWeight(.2f);
 
 		mainWindow.add(verticalDivider);
 
-		mainWindow.setJMenuBar(MenuUI.makeMenuBar(language, theme));
-
-		// mainWindow.pack();
 		mainWindow.setVisible(true);
 	}
 }
--- src/main/java/edu/kit/ipd/dbis/gui/StatusbarUI.java
+++ src/main/java/edu/kit/ipd/dbis/gui/StatusbarUI.java
@@ -7,12 +7,16 @@ package edu.kit.ipd.dbis.gui;
 import edu.kit.ipd.dbis.gui.theme.Theme;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ResourceBundle;
 
 public class StatusbarUI {
 	public static JPanel makeStatusbarUI(ResourceBundle language, Theme theme) {
 		JPanel statusbarUI = new JPanel();
 		statusbarUI.add(new JLabel("StatusbarUI"));
+
+		statusbarUI.setMaximumSize(new Dimension(Integer.MAX_VALUE, 10));
+		statusbarUI.setBorder(BorderFactory.createLineBorder(theme.foregroundColor, 1, false));
 		statusbarUI.setBackground(theme.backgroundColor);
 		statusbarUI.setForeground(theme.foregroundColor);
 		statusbarUI.setFont(theme.defaultFont);
