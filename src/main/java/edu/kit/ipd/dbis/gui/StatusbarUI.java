13f70d97860fdaf812b17c0737445132d904aaf4
Robin Link <MailRobinLink@gmail.com>
2018-01-04 01:11:15 +0100

    Filled the statusbar with it's components

--- src/main/java/edu/kit/ipd/dbis/gui/StatusbarUI.java
+++ src/main/java/edu/kit/ipd/dbis/gui/StatusbarUI.java
@@ -6,20 +6,61 @@ package edu.kit.ipd.dbis.gui;
 
 import edu.kit.ipd.dbis.gui.theme.Theme;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.ResourceBundle;
 
 public class StatusbarUI {
 	public static JPanel makeStatusbarUI(ResourceBundle language, Theme theme) {
-		JPanel statusbarUI = new JPanel();
-		statusbarUI.add(new JLabel("StatusbarUI"));
 
-		statusbarUI.setMaximumSize(new Dimension(Integer.MAX_VALUE, 10));
-		statusbarUI.setBorder(BorderFactory.createLineBorder(theme.foregroundColor, 1, false));
+		final int statusbarHeight = 15;
+
+		JPanel statusbarUI = new JPanel();
+		statusbarUI.setLayout(new BoxLayout(statusbarUI, BoxLayout.X_AXIS));
+		statusbarUI.add(makePauseButton(new Dimension(statusbarHeight, statusbarHeight), theme));
+		statusbarUI.add(Box.createHorizontalStrut(5));
+
+		JLabel statusText = new JLabel(language.getString("noDatabaseLoaded"));
+		statusbarUI.add(statusText);
+
+		statusbarUI.setMaximumSize(new Dimension(Integer.MAX_VALUE, statusbarHeight));
+		statusbarUI.setMinimumSize(new Dimension(Integer.MIN_VALUE, statusbarHeight));
+		statusbarUI.setBorder(BorderFactory.createLineBorder(theme.foregroundColor, 1));
 		statusbarUI.setBackground(theme.backgroundColor);
 		statusbarUI.setForeground(theme.foregroundColor);
-		statusbarUI.setFont(theme.defaultFont);
+
+		JButton log = new JButton("Log");
+		log.setBackground(theme.backgroundColor);
+		log.setForeground(theme.foregroundColor);
+		log.setFont(theme.defaultFont);
+		log.getSize(new Dimension(Integer.MAX_VALUE, statusbarHeight));
+		log.setBorder(BorderFactory.createEmptyBorder());
+
+		statusbarUI.add(Box.createHorizontalGlue());
+		statusbarUI.add(log);
+
 		return statusbarUI;
+	}
+
+	private static JButton makePauseButton(Dimension size, Theme theme) {
+		JButton pauseButton = new JButton();
+		pauseButton.setMaximumSize(size);
+		pauseButton.setBackground(theme.backgroundColor);
+		pauseButton.setForeground(theme.foregroundColor);
+		pauseButton.setBorder(BorderFactory.createEmptyBorder());
+
+		try {
+			Image icon = ImageIO.read(new File("src/main/resources/ButtonRun_Continue.png"));
+			pauseButton.setIcon(new ImageIcon(icon));
+		} catch (IOException e) {
+			System.out.println("Missing icon of button run!");
+			pauseButton.setText("P");
+			pauseButton.setFont(theme.defaultFont);
+		}
+
+		return pauseButton;
 	}
 }
