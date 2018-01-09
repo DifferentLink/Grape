ff9d21b7870d982838fdde2194e012cd78099f77
Robin Link <MailRobinLink@gmail.com>
2018-01-09 21:11:15 +0100

    Removed abstract from class AboutUI

--- src/main/java/edu/kit/ipd/dbis/gui/popups/AboutUI.java
+++ src/main/java/edu/kit/ipd/dbis/gui/popups/AboutUI.java
@@ -4,10 +4,8 @@
 
 package edu.kit.ipd.dbis.gui.popups;
 
-import edu.kit.ipd.dbis.gui.GUIWindow;
-
 import javax.swing.*;
 
-public abstract class AboutUI extends JFrame {
+public class AboutUI extends JFrame {
 
 }
