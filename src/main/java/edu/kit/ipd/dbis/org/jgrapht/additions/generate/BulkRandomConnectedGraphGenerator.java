634aed86f3eac64cabffa6e97a74ae89e6b4a8b2
Felix <koehlerfelix@arcor.de>
2018-01-11 19:05:05 +0100

    added methodheads, constructor and javadoc to alg.generate

--- src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/generate/BulkRandomConnectedGraphGenerator.java
+++ src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/generate/BulkRandomConnectedGraphGenerator.java
@@ -4,9 +4,20 @@ import org.jgrapht.Graph;
 
 import java.util.Set;
 
-public class BulkRandomConnectedGraphGenerator implements BulkGraphGenerator {
-	@Override
-	public void generateBulk(Set<Graph> target, int quantity, int minVertices, int maxVertices, int minEdges, int maxEdges) {
+/**
+ * A BulkGraphGenerator which creates a set of graphs that are strongly connected. The actual number of generated graphs
+ * may be lower than the specied one, as the given restrictions may limit the number of possible graphs.
+ * This class uses RandomConnectedGraphGenerator<V,E> to create each individual graph.
+ *
+ * @param <V> the graph vertex type
+ * @param <E> the graph edge type
+ */
+public class BulkRandomConnectedGraphGenerator<V, E> implements BulkGraphGenerator<V, E> {
 
+
+	@Override
+	public void generateBulk(Set<Graph> target, int quantity, int minVertices, int maxVertices, int minEdges,
+									int maxEdges) {
+		//Import algorithm
 	}
 }
