8cea59224e14995acc12f882b5fe2e009150f34c
Felix <koehlerfelix@arcor.de>
2018-01-11 13:48:26 +0100

    NextDenserGraphFinder javadoc added and method integration

--- src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/density/NextDenserGraphFinder.java
+++ src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/density/NextDenserGraphFinder.java
@@ -3,9 +3,33 @@ package edu.kit.ipd.dbis.org.jgrapht.additions.alg.density;
 import edu.kit.ipd.dbis.org.jgrapht.additions.alg.interfaces.NextDensityAlgorithm;
 import org.jgrapht.Graph;
 
-public class NextDenserGraphFinder<V,E> implements NextDensityAlgorithm {
+import java.util.Objects;
+
+/**
+ The next denser graph algorithm.
+ *
+ * @param <V> the graph vertex type
+ * @param <E> the graph edge type
+ */
+public class NextDenserGraphFinder<V, E> implements NextDensityAlgorithm {
+
+	/**
+	 * The input graph
+	 */
+	protected final Graph<V, E> graph;
+
+	/**
+	 * Construct a new next denser graph finder.
+	 *
+	 * @param graph the input graph
+	 */
+	public NextDenserGraphFinder(Graph<V, E> graph) {
+		this.graph = Objects.requireNonNull(graph, "Graph cannot be null");
+	}
+
 	@Override
 	public Graph getNextDensityGraph() {
+		//Import next denser graph algorithm
 		return null;
 	}
 }
