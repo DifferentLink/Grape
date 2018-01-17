8f5e1293e5a73fccd1e814b4959d28a6b7f03f57
Collin Lorbeer <collinlorbeer@freenet.de>
2018-01-17 16:30:47 +0100

    attributes of class BasicFilter are now in class Filter because class ConnectedFilter needs these attributes too

--- src/main/java/edu/kit/ipd/dbis/Controller/Filter/BasicFilter.java
+++ src/main/java/edu/kit/ipd/dbis/Controller/Filter/BasicFilter.java
@@ -6,47 +6,25 @@ import edu.kit.ipd.dbis.org.jgrapht.additions.graph.Property;
  * class which checks a specific attribute of a graph
  */
 public class BasicFilter extends Filter {
-    private int value;
-    private Relation relation;
-    private Property property;
 
     /**
      * Constructor of class BasicFilter
      * @param name name of the BasicFilter (the name should be identical to the user
      * input)
      * @param isActivated boolean which shows if the specific filter is activated
-     * @param value largest or smallest value which an attribute of a graph can have to
+     * @param value1 largest or smallest value which an attribute of a graph can have to
      * meet the criteria of the filter
-     * @param relation describes the relation between the property and the value
-     * @param property property the current filter inspects
+     * @param relation1 describes the relation between the property and the value
+     * @param property1 property the current filter inspects
      */
-    BasicFilter(String name, boolean isActivated, int value,
-                Relation relation, Property property) {
+    BasicFilter(String name, boolean isActivated, int value1,
+                Relation relation1, Property property1) {
+        this.name = name;
+        this.property1 = property1;
+        this.isActivated = isActivated;
+        this.value1 = value1;
+        this.relation1 = relation1;
 
-    }
-
-    /**
-     * getter of attribute value
-     * @return integer value of attribute value
-     */
-    public int getValue() {
-        return value;
-    }
-
-    /**
-     * getter of attribute relation
-     * @return value of attribute relation
-     */
-    public Relation getRelation() {
-        return relation;
-    }
-
-    /**
-     * getter of attribute property
-     * @return value of attribute property
-     */
-    public Property getProperty() {
-        return property;
     }
 
     @Override
--- src/main/java/edu/kit/ipd/dbis/Controller/Filter/Filter.java
+++ src/main/java/edu/kit/ipd/dbis/Controller/Filter/Filter.java
@@ -1,9 +1,39 @@
 package edu.kit.ipd.dbis.Controller.Filter;
 
+import edu.kit.ipd.dbis.org.jgrapht.additions.graph.Property;
+
 /**
  * a specific filter might be a BasicFilter or a ConnectedFilter
  */
 public abstract class Filter extends Filtersegment {
+
+    protected int value1;
+    protected Relation relation1;
+    protected Property property1;
+
+    /**
+     * getter of attribute value1
+     * @return integer value of attribute value1
+     */
+    public int getValue() {
+        return value1;
+    }
+
+    /**
+     * getter of attribute relation1
+     * @return value of attribute relation1
+     */
+    public Relation getRelation1() {
+        return relation1;
+    }
+
+    /**
+     * getter of attribute property1
+     * @return value of attribute property1
+     */
+    public Property getProperty() {
+        return property1;
+    }
 
     /**
      * checks if a specific graph meets every criteria of the current filter
