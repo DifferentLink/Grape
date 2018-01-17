8b7ce5ae1219099feaabc87e0699791c28ba9db9
Felix <koehlerfelix@arcor.de>
2018-01-17 16:45:20 +0100

    new begin of minimal bfs code algorithm

--- src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/density/MinimalBfsCodeAlgorithm.java
+++ src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/density/MinimalBfsCodeAlgorithm.java
@@ -3,7 +3,11 @@ package edu.kit.ipd.dbis.org.jgrapht.additions.alg.density;
 import edu.kit.ipd.dbis.org.jgrapht.additions.alg.interfaces.BfsCodeAlgorithm;
 import edu.kit.ipd.dbis.org.jgrapht.additions.graph.PropertyGraph;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  The minimal bfs code algorithm.
@@ -39,56 +43,63 @@ public class MinimalBfsCodeAlgorithm<V, E> implements BfsCodeAlgorithm {
 	 * @param startNode
 	 * @return
 	 */
-	public BfsCode getLocalBfsCode(PropertyGraph graph, V startNode, int nodeCounter, Map<V,Integer> bfsIds) {
-		nodeCounter++;
-		BfsCode target = new BfsCodeImpl(new int[0]);
-		if (nodeCounter >= graph.vertexSet().size()) {
-			return new BfsCodeImpl(calculateBFS(graph, bfsIds));
-		}
-		List<V> adjacentNodes = new ArrayList<>();
-		for (Object e : graph.outgoingEdgesOf(startNode)) {
-			if (!bfsIds.containsValue(graph.getEdgeTarget(e))) {
-				adjacentNodes.add((V) graph.getEdgeTarget(e));
-			}
-		}
+	public BfsCode getLocalBfsCode(PropertyGraph graph, V startNode) {
+		int nodeCounter = 1;
 
-		List<List<V>> bestPermutations = new ArrayList<>();
-		int[] bestCode = new int[3 * graph.edgeSet().size()];
-		for (int i : bestCode) {
-			i = Integer.MAX_VALUE;
-		}
-		//calculate bfs code for the subgraph of adjacentNodes + nodes from bfsIds;
-		for (List<V> perm : getPermutations(adjacentNodes)) {
-			//new map for subgraph
-			Map<V, Integer> subgraphMap = new HashMap<>();
-			subgraphMap.putAll(bfsIds);
-			for(int i = 0; i < perm.size(); i++) {
-				subgraphMap.put(perm.get(i), i + 1 + nodeCounter);
-			}
-			int[] bfsCode = calculateBFS(graph, subgraphMap);
-			if (compareLocal(bfsCode, bestCode) == 1) {
-				bestCode = bfsCode;
-				bestPermutations.clear();
-				bestPermutations.add(perm);
-			} else if (compareLocal(bfsCode, bestCode) == 0) {
-				bestPermutations.add(perm);
-			}
-		}
+		Map<V,Integer> bfsIds = new HashMap<>();
+		bfsIds.put(startNode, 1);
 
-		for (List<V> bestperm : bestPermutations) {
-			Map<V, Integer> subgraphMap = new HashMap<>();
-			subgraphMap.putAll(bfsIds);
-			for(int i = 0; i < bestperm.size(); i++) {
-				subgraphMap.put(bestperm.get(i), i + 1 + nodeCounter);
+		V actualNode = startNode;
+
+		while(nodeCounter < graph.vertexSet().size() && bfsIds.size() < graph.vertexSet().size()) {
+
+			List<V> adjacentNotCheckedNodes = new ArrayList<>();
+			//computes the adjacent nodes that are not checked yet
+			for (Object e : graph.outgoingEdgesOf(actualNode)) {
+				Object adjacentNode1 = graph.getEdgeTarget(e);
+				Object adjacentNode2 = graph.getEdgeSource(e);
+				if (!bfsIds.containsValue(adjacentNode1)) {
+					adjacentNotCheckedNodes.add((V) adjacentNode1);
+				}
+				if (!bfsIds.containsValue(adjacentNode2)) {
+					adjacentNotCheckedNodes.add((V) adjacentNode2);
+				}
 			}
-			if (bfsIds.size() >= graph.vertexSet().size()) {
-				return new BfsCodeImpl(calculateBFS(graph, subgraphMap));
+	/**
+			List<List<V>> bestPermutations = new ArrayList<>();
+			int[] bestCode = new int[3 * graph.edgeSet().size()];
+			for (int i : bestCode) {
+				i = Integer.MAX_VALUE;
+			}
+			//calculate bfs code for the subgraph of adjacentNodes + nodes from bfsIds;
+			for (List<V> perm : getPermutations(adjacentNodes)) {
+				//new map for subgraph
+				Map<V, Integer> subgraphMap = new HashMap<>();
+				subgraphMap.putAll(bfsIds);
+				for(int i = 0; i < perm.size(); i++) {
+					subgraphMap.put(perm.get(i), i + 1 + nodeCounter);
+				}
+				int[] bfsCode = calculateBFS(graph, subgraphMap);
+				if (compareLocal(bfsCode, bestCode) == 1) {
+					bestCode = bfsCode;
+					bestPermutations.clear();
+					bestPermutations.add(perm);
+				} else if (compareLocal(bfsCode, bestCode) == 0) {
+					bestPermutations.add(perm);
+				}
 			}
 
-			target = getLocalBfsCode(graph, bestperm.get(0), nodeCounter,subgraphMap);
+			for (List<V> bestperm : bestPermutations) {
+				Map<V, Integer> subgraphMap = new HashMap<>();
+				subgraphMap.putAll(bfsIds);
+				for(int i = 0; i < bestperm.size(); i++) {
+					subgraphMap.put(bestperm.get(i), i + 1 + nodeCounter);
+				}
+
+			}
+			 */
 		}
-
-		return target;
+		return new BfsCodeImpl(calculateBFS(graph, bfsIds));
 	}
 
 	private int compareLocal(int[] bfs1, int[] bfs2) {
