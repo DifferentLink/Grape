3a4dd8408dcf6248010ac8022e1157735ea80d11
Cem Özcan <uvtwy@student.kit.edu>
2018-01-18 15:39:07 +0100

    Class "Parser": Method "parse()" throws an exception

--- src/main/java/edu/kit/ipd/dbis/database/LoadParser.java
+++ src/main/java/edu/kit/ipd/dbis/database/LoadParser.java
@@ -14,7 +14,7 @@ public class LoadParser extends Parser {
 	}
 
 	@Override
-	public void parse() {
+	public void parse() throws Exception {
 		String[] content = this.information.split(";");
 		GraphTable graphTable = new GraphTable(content[0], content[1], content[2], content[3]);
 		FilterTable filterTable = new FilterTable(content[4], content[5],content[6],content[7]);
