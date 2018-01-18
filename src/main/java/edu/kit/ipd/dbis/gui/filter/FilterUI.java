453bc8fd0d261e53437e01a6abee8aec79fbf22c
Robin Link <MailRobinLink@gmail.com>
2018-01-18 20:03:58 +0100

    Improved GUI rendering

--- src/main/java/edu/kit/ipd/dbis/gui/filter/FilterUI.java
+++ src/main/java/edu/kit/ipd/dbis/gui/filter/FilterUI.java
@@ -15,34 +15,50 @@ import java.util.ResourceBundle;
 public class FilterUI extends GUIElement {
 	private FilterManagement filterManagement;
 	private JPanel filterMenu;
-	private JPanel filterGroupsUI;
-	private JPanel simpleFilterUI;
+	private JPanel filter;
 
 	public FilterUI(Controller controller, ResourceBundle language, Theme theme) {
 		super(controller, language, theme);
 
 		filterManagement = new FilterManagement();
 
-		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.setLayout(new BorderLayout());
 		this.setBackground(theme.backgroundColor);
 		this.setForeground(theme.foregroundColor);
 
 		filterMenu = new JPanel();
 		filterMenu.setLayout(new BoxLayout(filterMenu, BoxLayout.X_AXIS));
 		String[] filterMenuEntries = {"Save selected filter...", "Load Filter..."}; // todo use language resource
-		JComboBox<String> filter = new JComboBox<>(filterMenuEntries);
-		filter.setMaximumSize(new Dimension(100, 100));
-		filter.setMinimumSize(new Dimension(100, 0));
-		filter.setBackground(theme.backgroundColor);
-		filter.setForeground(theme.foregroundColor);
-		filterMenu.add(filter);
+		JComboBox<String> filterDropdown = new JComboBox<>(filterMenuEntries);
+		filterDropdown.setMaximumSize(new Dimension(100, 100));
+		filterDropdown.setMinimumSize(new Dimension(100, 0));
+		filterDropdown.setBackground(theme.backgroundColor);
+		filterDropdown.setForeground(theme.foregroundColor);
+		filterMenu.add(filterDropdown);
 
 		this.add(filterMenu, BorderLayout.NORTH);
+
+		filter = new JPanel();
+		this.add(filter, BorderLayout.CENTER);
 
 		JPanel buttons = new JPanel();
 		buttons.setLayout(new BoxLayout(buttons, BoxLayout.X_AXIS));
 		buttons.setBackground(theme.backgroundColor);
 		buttons.setForeground(theme.foregroundColor);
+
+		JButton newFilter = new JButton(" New Filter "); // todo replace with string from language
+		JButton newFilterGroup = new JButton(" New Group "); // todo replace with string from language
+		theme.style(newFilter);
+		theme.style(newFilterGroup);
+		buttons.add(newFilter);
+		buttons.add(Box.createHorizontalStrut(5));
+		buttons.add(newFilterGroup);
+		buttons.setBorder(BorderFactory.createLineBorder(theme.foregroundColor, 1));
+
+		this.add(Box.createVerticalGlue());
+		this.add(buttons, BorderLayout.SOUTH);
+
+		this.setBorder(BorderFactory.createLineBorder(Color.lightGray, 2));
 	}
 
 	/**
@@ -51,15 +67,15 @@ public class FilterUI extends GUIElement {
 	@Override
 	public void update() {
 
-		filterGroupsUI = new JPanel();
-		filterGroupsUI.setLayout(new BoxLayout(filterGroupsUI, BoxLayout.Y_AXIS));
+		filter = new JPanel();
+		filter.setLayout(new BoxLayout(filter, BoxLayout.Y_AXIS));
 
 		for (FilterGroup filterGroup : filterManagement.getFilterGroups()) {
-			filterGroupsUI.add(drawFilterGroup(filterGroup));
+			filter.add(drawFilterGroup(filterGroup));
 		}
 
 		for (SimpleFilter simpleFilter : filterManagement.getSimpleFilter()) {
-			simpleFilterUI.add(drawSimpleFilter(simpleFilter));
+			filter.add(drawSimpleFilter(simpleFilter));
 		}
 	}
 
