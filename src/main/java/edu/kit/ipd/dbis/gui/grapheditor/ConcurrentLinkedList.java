22f691d8fecd71d8899f53b43312955476204168
Robin Link <MailRobinLink@gmail.com>
2018-01-19 00:25:08 +0100

    Added more methods to concurrent list

--- src/main/java/edu/kit/ipd/dbis/gui/grapheditor/ConcurrentLinkedList.java
+++ src/main/java/edu/kit/ipd/dbis/gui/grapheditor/ConcurrentLinkedList.java
@@ -27,19 +27,35 @@ public class ConcurrentLinkedList<T> {
 		return head == head.getNext();
 	}
 
-	public synchronized ConcurrentNode<T> popBack() {
+	public synchronized T popBack() {
 		ConcurrentNode<T> out = head.getPrevious();
 		head.getPrevious().setNext(null);
 		head.getPrevious().setPrevious(null);
 		head.setPrevious(out.getPrevious());
-		return out;
+		return out.get();
 	}
 
-	public synchronized ConcurrentNode<T> popFront() {
+	public synchronized T popFront() {
 		ConcurrentNode<T> out = head.getPrevious();
 		head.getNext().setNext(null);
 		head.getNext().setPrevious(null);
 		head.setNext(out.getNext());
-		return out;
+		return out.get();
+	}
+
+	public synchronized void pushFront(T element) {
+		ConcurrentNode<T> newNode = new ConcurrentNode<>(element);
+		head.getNext().setPrevious(newNode);
+		newNode.setNext(head.getNext());
+		newNode.setPrevious(head);
+		head.setNext(newNode);
+	}
+
+	public synchronized void pushBack(T element) {
+		ConcurrentNode<T> newNode = new ConcurrentNode<>(element);
+		head.getPrevious().setNext(newNode);
+		newNode.setNext(head);
+		newNode.setPrevious(head.getPrevious());
+		head.setPrevious(newNode);
 	}
 }
