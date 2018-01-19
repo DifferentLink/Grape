800abd1009823cd1f78cca070203a11513c3d8c1
Robin Link <MailRobinLink@gmail.com>
2018-01-19 17:27:20 +0100

    Added more funtionality to ConcurrentLinkedList

--- src/main/java/edu/kit/ipd/dbis/gui/grapheditor/util/ConcurrentLinkedList.java
+++ src/main/java/edu/kit/ipd/dbis/gui/grapheditor/util/ConcurrentLinkedList.java
@@ -4,12 +4,27 @@
 
 package edu.kit.ipd.dbis.gui.grapheditor.util;
 
-public class ConcurrentLinkedList<T> {
-	private ConcurrentNode<T> head = new ConcurrentNode<>(null);
+import java.util.Iterator;
 
+public class ConcurrentLinkedList<T> {
+	private ConcurrentNode<T> head;
+
+	public ConcurrentLinkedList() {
+		head = new ConcurrentNode<>(null);
+		head.setNext(head);
+		head.setPrevious(head);
+	}
+
+	public synchronized ConcurrentNode<T> head() {
+		return this.head;
+	}
 
 	public synchronized T getFirst() {
 		return head.getNext().get();
+	}
+
+	public synchronized T getLast() {
+		return head.getPrevious().get();
 	}
 
 	public synchronized ConcurrentNode<T> first() {
@@ -54,5 +69,39 @@ public class ConcurrentLinkedList<T> {
 		newNode.setNext(head);
 		newNode.setPrevious(head.getPrevious());
 		head.setPrevious(newNode);
+	}
+
+	public synchronized int size() {
+		int size = 0;
+
+		Iterator<T> iterator = this.iterator();
+		while (iterator.next() != null) {
+			size++;
+		}
+
+		return size;
+	}
+
+	public synchronized Iterator<T> iterator() {
+		return new ConcurrentIterator<>(this);
+	}
+
+	private class ConcurrentIterator<E> implements Iterator<E> {
+
+		private ConcurrentNode<E> pointer;
+
+		private ConcurrentIterator(ConcurrentLinkedList<E> list) {
+			this.pointer = list.head();
+		}
+
+		@Override
+		public boolean hasNext() {
+			return false;
+		}
+
+		@Override
+		public E next() {
+			return null;
+		}
 	}
 }
