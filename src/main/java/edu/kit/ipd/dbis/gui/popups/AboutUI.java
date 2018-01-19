caed06afc668561d2f186fc56e59a8e5c42d9d82
Robin Link <MailRobinLink@gmail.com>
2018-01-19 23:47:15 +0100

    About window now uses Grape logo

--- src/main/java/edu/kit/ipd/dbis/gui/popups/AboutUI.java
+++ src/main/java/edu/kit/ipd/dbis/gui/popups/AboutUI.java
@@ -5,13 +5,18 @@
 package edu.kit.ipd.dbis.gui.popups;
 
 import edu.kit.ipd.dbis.gui.themes.Theme;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class AboutUI extends JFrame {
 	public AboutUI(Theme theme) {
 		this.setBackground(theme.backgroundColor);
-		this.setIconImage(null);
+		try {
+			this.setIconImage(ImageIO.read(getClass().getResource("/icons/GrapeLogo.png")));
+		} catch (IOException ignored) {}
 		JPanel container = new JPanel(new BorderLayout());
 		container.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 		JLabel aboutText = new JLabel("About text");
