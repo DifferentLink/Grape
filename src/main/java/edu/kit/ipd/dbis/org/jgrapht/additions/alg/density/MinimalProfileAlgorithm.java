ff93e8428a7e3368370cdfa9001e50e15fecd8c6
Felix <koehlerfelix@arcor.de>
2018-01-19 01:34:37 +0100

    implemented getProfil method in minimalProfilAlgorithm

--- src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/density/MinimalProfileAlgorithm.java
+++ src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/density/MinimalProfileAlgorithm.java
@@ -3,6 +3,7 @@ package edu.kit.ipd.dbis.org.jgrapht.additions.alg.density;
 import edu.kit.ipd.dbis.org.jgrapht.additions.alg.interfaces.BfsCodeAlgorithm;
 import edu.kit.ipd.dbis.org.jgrapht.additions.alg.interfaces.ProfileDensityAlgorithm;
 import edu.kit.ipd.dbis.org.jgrapht.additions.graph.PropertyGraph;
+import edu.kit.ipd.dbis.org.jgrapht.additions.graph.properties.complex.BfsCode;
 
 import java.util.*;
 
@@ -16,11 +17,21 @@ public class MinimalProfileAlgorithm<V, E> implements ProfileDensityAlgorithm {
 
 	@Override
 	public Profile getProfile(PropertyGraph graph) {
-		ArrayList<BfsCodeAlgorithm.BfsCode> allLocalCodes = new ArrayList<>();
+		LinkedList<BfsCodeAlgorithm.BfsCode> allLocalCodes = new LinkedList<>(); //is sorted at the end
 		for (Object v : graph.vertexSet()) {
 			LocalBfsCodeAlgorithm local = new LocalBfsCodeAlgorithm((V) v);
 			BfsCodeAlgorithm.BfsCode localCode = local.getBfsCode(graph);
-			allLocalCodes.add(localCode);
+
+			//adds the localCode at the right sorted order
+			Iterator<BfsCodeAlgorithm.BfsCode> iterator = allLocalCodes.iterator();
+			int index = 0;
+			while (iterator.hasNext()) {
+				BfsCodeAlgorithm.BfsCode code = iterator.next();
+				if (code.compareTo(localCode) < 0) {
+					index++;
+				}
+			}
+			allLocalCodes.add(index, localCode);
 		}
 		return new ProfileImpl(getProfile(allLocalCodes));
 	}
@@ -30,7 +41,18 @@ public class MinimalProfileAlgorithm<V, E> implements ProfileDensityAlgorithm {
 	 * @param list the list
 	 * @return the profil
 	 */
-	public int[][] getProfile(ArrayList<BfsCodeAlgorithm.BfsCode> list) {
-		return null;
+	public int[][] getProfile(LinkedList<BfsCodeAlgorithm.BfsCode> list) {
+		if (list.size() == 0) {
+			return new int[0][0];
+		}
+		int[][] result = new int[list.size()][];
+		int i = 0;
+		Iterator<BfsCodeAlgorithm.BfsCode> iterator = list.iterator();
+		while (iterator.hasNext()) {
+			int[] code = iterator.next().getCode();
+			result[i] = code;
+			i++;
+		}
+		return result;
 	}
 }
