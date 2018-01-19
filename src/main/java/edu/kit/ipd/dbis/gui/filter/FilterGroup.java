11d340e84cbb6653a022557c4e4740bec398ef3b
Robin Link <MailRobinLink@gmail.com>
2018-01-19 22:45:36 +0100

    Removed imports

--- src/main/java/edu/kit/ipd/dbis/gui/filter/FilterGroup.java
+++ src/main/java/edu/kit/ipd/dbis/gui/filter/FilterGroup.java
@@ -5,9 +5,7 @@
 package edu.kit.ipd.dbis.gui.filter;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class FilterGroup extends Filter {
 
