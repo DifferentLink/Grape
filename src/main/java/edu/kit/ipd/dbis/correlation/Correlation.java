1e18bc0541bcde435c2242490e31b41015c73c36
Robin Link <MailRobinLink@gmail.com>
2018-01-26 12:13:04 +0100

    Squashed commit of the following:
    commit 9f385626a59c8bf98c70985cc947e26c07466a29
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Fri Jan 26 10:28:09 2018 +0100
    
        to seperate maximum and minimum the abstract class Correlation has
        now two abstract methods useMaximum and useMinimum
    
    commit cb8fa804bc77d8e92a2665172573478cf967af82
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Fri Jan 26 10:01:38 2018 +0100
    
        added documentation to methods of class CorrelationRequest
    
    commit ca82b57e62fe5d06a6c3b9a35eb95afb948d6094
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Thu Jan 25 17:33:33 2018 +0100
    
        added javadoc to class InvalidFilterInputException
    
    commit 72445fc9aab001420d17662a3ab819e635d35465
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Thu Jan 25 17:20:10 2018 +0100
    
        added method use() to classes Correlation, CorrelationRequest,
        MutualCorrelation und Pearson. The method is used to calculate the
        specific correlations
    
    commit d6856ee14cf5914ea2e37411daf327c184231895
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Thu Jan 25 17:00:52 2018 +0100
    
        implemented parser in class CorrelationRequest which parses a user
        input to a correlation
    
    commit 0e9c92baefee1b0e9a2efc1803afa0558a4e8f99
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Thu Jan 25 13:36:19 2018 +0100
    
        implemented setter and getter of abstract class Correlation
    
    commit c4f61de994dd4ad180f1dd00b849e50a6de98d55
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Thu Jan 25 11:10:23 2018 +0100
    
        implemented parser for a correlation request
    
    commit ce501f6346177c201b8a16b2a0a74036bc901342
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Thu Jan 25 11:09:48 2018 +0100
    
        classes Pearson and MutualCorrelation are now subclasses of abstract
        class correlation
    
    commit 7ad9e712657aaf5277876c6873dbbece16f1c1fd
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Thu Jan 25 11:08:09 2018 +0100
    
        added new class InvalidCorrelationInputException which is used to
        throw an exception if the input string for a correlation is not valid
    
    commit 275ae83144ced07ce5f0e8bb4dfc78f69b9f5b45
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Thu Jan 25 10:09:08 2018 +0100
    
        created methods getMaximalCorrelation() and getMinimalCorrelation
        in class Pearson
    
    commit dcf476ba68e740e2f27e1a58cded33f1b5a5a2df
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Thu Jan 25 10:08:13 2018 +0100
    
        created constructor in class CorrelationOutput
    
    commit dde260b8f317019161e78cc5f88fa13b1f149939
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Thu Jan 25 10:06:55 2018 +0100
    
        class Correlation is abstract now
    
    commit f5a4bc2396c8173131160dfc298b6f09cac2bbe6
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Wed Jan 24 16:04:01 2018 +0100
    
        access to the attributes is private now
    
    commit 3dd9abcbb56585f42d36d7868d5f78eb64ab6565
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Mon Jan 22 12:33:08 2018 +0100
    
        wrote attributes for abstract class Correlation
    
    commit b39c4c57cc33d94da8ebbd16a9a08a8e09afd995
    Author: Collin Lorbeer <collinlorbeer@freenet.de>
    Date:   Mon Jan 22 12:15:29 2018 +0100
    
        created classes of package correlation and implemented methods of
        class CorrelationOutput

---
+++ src/main/java/edu/kit/ipd/dbis/correlation/Correlation.java
@@ -0,0 +1,81 @@
+package edu.kit.ipd.dbis.correlation;
+
+import edu.kit.ipd.dbis.org.jgrapht.additions.graph.Property;
+
+import java.util.ArrayList;
+
+/**
+ * abstract class which inherits all attributes and methods every correlation
+ * should implement
+ */
+abstract class Correlation {
+    private int attributeCounter;
+    private Property property;
+    private boolean maximum;
+
+    /**
+     * method which is used to check filters for a specific correlation
+     * @return returns a list of objects of class CorrelationOutput which code the results of the
+     * correlation calculation
+     */
+    public abstract ArrayList<CorrelationOutput> useMaximum();
+
+    /**
+     * method which is used to check filters for a specific correlation
+     * @return returns a list of objects of class CorrelationOutput which code the results of the
+     * correlation calculation
+     */
+    public abstract ArrayList<CorrelationOutput> useMinimum();
+
+    /**
+     * setter of attribute attributeCounter
+     * @param attributeCounter integer value which inherits the information
+     * how many pairs of properties are wanted
+     */
+    void setAttributeCounter(int attributeCounter) {
+        this.attributeCounter = attributeCounter;
+    }
+
+    /**
+     * setter of attribute property
+     * @param property specific property the correlation should focus on
+     */
+    void setProperty(Property property) {
+        this.property = property;
+    }
+
+    /**
+     * setter of attribute maximum
+     * @param maximum shows if it is searched for the weakest or for
+     * the strongest correlations
+     */
+    void setMaximum(boolean maximum) {
+        this.maximum = maximum;
+    }
+
+    /**
+     * getter of attribute attributeCounter
+     * @return integer value which inherits the information
+     * how many pairs of properties are wanted
+     */
+    int getAttributeCounter() {
+        return attributeCounter;
+    }
+
+    /**
+     * getter of attribute property
+     * @return returns the property the specific correlation should focus on
+     */
+    Property getProperty() {
+        return property;
+    }
+
+    /**
+     * getter of attribute maximum
+     * @return return true if it is search for a strong correlation and false
+     * if it is searched for a weak correlation
+     */
+    boolean getMaximum() {
+        return maximum;
+    }
+}
---
+++ src/main/java/edu/kit/ipd/dbis/correlation/CorrelationOutput.java
@@ -0,0 +1,48 @@
+package edu.kit.ipd.dbis.correlation;
+
+import edu.kit.ipd.dbis.org.jgrapht.additions.graph.Property;
+
+/**
+ * class which allows the GUI to get a list of relevant correlations
+ */
+public class CorrelationOutput {
+    private Property firstProperty;
+    private Property secondProperty;
+    private double outputNumber;
+
+    /**
+     * Constructor of class CorelationOutput
+     * @param firstProperty first property of a specific correlation
+     * @param secondProperty second property of a specific correlation
+     * @param outputNumber double value which shows how strong a specific correlation is
+     */
+    public CorrelationOutput(Property firstProperty, Property secondProperty, double outputNumber) {
+        this.firstProperty = firstProperty;
+        this.secondProperty = secondProperty;
+        this.outputNumber = outputNumber;
+    }
+
+    /**
+     * used to get the first property of a specific correlation
+     * @return retuns the first property of a specific correlation
+     */
+    public Property getFirstProperty() {
+        return firstProperty;
+    }
+
+    /**
+     * used to get the second property of a specific correlation
+     * @return retuns the second property of a specific correlation
+     */
+    public Property getSecondProperty() {
+        return secondProperty;
+    }
+
+    /**
+     * used to get a number which shows how strong the correlation is
+     * @return retuns a number which shows how strong a specific correlation is
+     */
+    public double getOutputNumber() {
+        return outputNumber;
+    }
+}
