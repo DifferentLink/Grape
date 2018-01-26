59905bb972dbba26beaa55635a05daee7094c477
Robin Link <MailRobinLink@gmail.com>
2018-01-26 12:29:45 +0100

    Master cleanup # Conflicts: #	src/main/java/edu/kit/ipd/dbis/database/connection/GraphDatabase.java #	src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/graph/Property.java #	src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/graph/properties/complex/BfsCode.java #	src/test/java/edu/kit/ipd/dbis/filter/FiltergroupTest.java #	src/test/java/edu/kit/ipd/dbis/filter/FiltermanagementTest.java #	src/test/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/density/MinimalBfsCodeAlgorithmTest.java

---
+++ src/main/java/edu/kit/ipd/dbis/filter/InvalidInputException.java
@@ -0,0 +1,9 @@
+package edu.kit.ipd.dbis.filter;
+
+/**
+ * an InvalidInputException is thrown when the user tries to add a new filter or a new filtergroup but uses wrong
+ * syntax
+ */
+public class InvalidInputException extends Throwable {
+
+}
