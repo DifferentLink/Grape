abf7b5eb409b0a4c5c939c8b813eb779b69a8911
Collin Lorbeer <collinlorbeer@freenet.de>
2018-01-28 17:02:12 +0100

    Changed header of methods useMaximum() and useMinimum() in class MutualCorrelation and added methods useMaximum(Property property1) and useMinimum(Property property1) to class MutualCorrelation

--- src/main/java/edu/kit/ipd/dbis/correlation/MutualCorrelation.java
+++ src/main/java/edu/kit/ipd/dbis/correlation/MutualCorrelation.java
@@ -1,16 +1,30 @@
 package edu.kit.ipd.dbis.correlation;
 
+import edu.kit.ipd.dbis.org.jgrapht.additions.graph.Property;
+import sun.reflect.generics.tree.Tree;
+
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class MutualCorrelation extends Correlation {
 
     @Override
-    public ArrayList<CorrelationOutput> useMaximum() {
+    public TreeSet<CorrelationOutput> useMaximum() {
         return null;
     }
 
     @Override
-    public ArrayList<CorrelationOutput> useMinimum() {
+    public TreeSet<CorrelationOutput> useMaximum(Property property1) {
+        return null;
+    }
+
+    @Override
+    public TreeSet<CorrelationOutput> useMinimum() {
+        return null;
+    }
+
+    @Override
+    public TreeSet<CorrelationOutput> useMinimum(Property property1) {
         return null;
     }
 }
