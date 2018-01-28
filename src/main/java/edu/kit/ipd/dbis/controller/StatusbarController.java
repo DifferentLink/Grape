773e55699fb681983a806bdcf2bb406363591c7e
Alexander Schorn <Alexander.Schorn@gmx.net>
2018-01-28 18:23:21 +0100

    Statusbar: unecessary method removed.

--- src/main/java/edu/kit/ipd/dbis/controller/StatusbarController.java
+++ src/main/java/edu/kit/ipd/dbis/controller/StatusbarController.java
@@ -36,15 +36,6 @@ public class StatusbarController {
 	}
 
 	/**
-	 * Parses a given String to the current history object with the given events.
-	 *
-	 * @param historyString the string of the history.
-	 */
-	private void parseHistory(String historyString) {
-
-	}
-
-	/**
 	 * Removes all events of the type MESSAGE from the current history
 	 */
 	public void removeMessages() {
