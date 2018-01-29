d201b41b802e1cd926efe0c7db01dc6c43c17485
Gereon Recht <gereon.recht@protonmail.com>
2018-01-29 18:53:02 +0100

    Implemented asPropertyGraph().

--- src/test/java/edu/kit/ipd/dbis/gui/grapheditor/RenderableGraphTest.java
+++ src/test/java/edu/kit/ipd/dbis/gui/grapheditor/RenderableGraphTest.java
@@ -25,5 +25,16 @@ public class RenderableGraphTest {
 
 	@Test
 	public void asPropertyGraph() {
+		PropertyGraph graph = new PropertyGraph();
+		graph.addVertex(0);
+		graph.addVertex(1);
+		graph.addVertex(2);
+		graph.addVertex(3);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 2);
+		graph.addEdge(1, 2);
+		graph.addEdge(1, 3);
+		RenderableGraph renderableGraph = new RenderableGraph(graph);
+		assertEquals(true, graph.equals(renderableGraph.asPropertyGraph()));
 	}
 }
