56eed9b9d2a6301b0556e6724e3cb9a222f1218f
Felix <koehlerfelix@arcor.de>
2018-01-29 23:07:39 +0100

    added getNextCombination method to KkGraphGenerator

--- src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/kkgraph/KkGraphGenerator.java
+++ src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/kkgraph/KkGraphGenerator.java
@@ -1,7 +1,10 @@
 package edu.kit.ipd.dbis.org.jgrapht.additions.alg.kkgraph;
 
 import edu.kit.ipd.dbis.org.jgrapht.additions.alg.interfaces.KkGraphAlgorithm;
+import edu.kit.ipd.dbis.org.jgrapht.additions.graph.Property;
 import edu.kit.ipd.dbis.org.jgrapht.additions.graph.PropertyGraph;
+import edu.kit.ipd.dbis.org.jgrapht.additions.graph.properties.complex.VertexColoring;
+import org.jgrapht.alg.interfaces.VertexColoringAlgorithm;
 
 import java.util.Objects;
 
@@ -28,8 +31,66 @@ public class KkGraphGenerator<V, E> implements KkGraphAlgorithm {
 
 	@Override
 	public KkGraph getKkGraph() {
+		VertexColoringAlgorithm.ColoringImpl vertexColoring = (VertexColoringAlgorithm.ColoringImpl)
+				graph.getProperty(VertexColoring.class).getValue();
+		int numberOfContractEdges = graph.vertexSet().size() - vertexColoring.getNumberColors();
+
+
 
 		//TODO: implement me (empty implementation for test in PropertyGraphTest)
 		return new KkGraphImpl(null, 2);
+	}
+
+	private void contractEdge(PropertyGraph graph, V firstNde, V secondNode) {
+	}
+
+
+	/**
+	 * calculates the next combination of a combination
+	 * @param prevComb the previous combination
+	 * @return the next combination
+	 */
+	public int[] getNextEdgeCombination(int[] prevComb) {
+		for (int i = 0; i < prevComb.length; i++) {
+			if (!(prevComb[i] == 1 || prevComb[i] == 0)) {
+				throw new IllegalArgumentException("no valid comb");
+			}
+		}
+
+		int[] result = new int[prevComb.length];
+
+		boolean found = false;
+		int onesBeforeMoveableOne = 0;
+		if (prevComb[prevComb.length - 1] == 1) {
+			onesBeforeMoveableOne++;
+		}
+		int moveableOneIndex = 0;
+		int i = prevComb.length - 2;
+		while(!found && i > 0) {
+			if (prevComb[i] == 1 && prevComb[i + 1] == 0) {
+				found = true;
+				moveableOneIndex = i;
+			} else if (prevComb[i] == 1) {
+				onesBeforeMoveableOne++;
+			}
+			i--;
+		}
+		for (int j = 0; j < moveableOneIndex; j++) {
+			result[j] = prevComb[j];
+		}
+		result[moveableOneIndex] = 0;
+		result[moveableOneIndex + 1] = 1;
+		if (moveableOneIndex + 2 < result.length) {
+
+			for (int j = moveableOneIndex + 2; j < result.length; j++) {
+				if (onesBeforeMoveableOne > 0) {
+					result[j] = 1;
+				} else {
+					result[j] = 0;
+				}
+				onesBeforeMoveableOne--;
+			}
+		}
+		return result;
 	}
 }
--- src/test/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/kkgraph/KkGraphGeneratorTest.java
+++ src/test/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/kkgraph/KkGraphGeneratorTest.java
@@ -1,5 +1,32 @@
 package edu.kit.ipd.dbis.org.jgrapht.additions.alg.kkgraph;
 
+import edu.kit.ipd.dbis.org.jgrapht.additions.graph.PropertyGraph;
+import org.junit.Test;
+
 public class KkGraphGeneratorTest {
-	
+
+	@Test
+	public void Test() {
+		PropertyGraph graph = new PropertyGraph();
+		KkGraphGenerator g = new KkGraphGenerator(graph);
+		int[] a = {1,1,1,0,0};
+		for (int i = 0; i < 9; i++) {
+			int[] b = g.getNextEdgeCombination(a);
+			for (int j = 0; j < b.length; j++) {
+				System.out.print(b[j] +  ", ");
+			}
+			System.out.println();
+			a = b;
+		}
+	}
+	@Test
+	public void Test1() {
+		PropertyGraph graph = new PropertyGraph();
+		KkGraphGenerator g = new KkGraphGenerator(graph);
+		int[] a = {1,1,1,0,0,1,1,1};
+		int[] b = g.getNextEdgeCombination(a);
+		for (int j = 0; j < b.length; j++) {
+			System.out.print(b[j] +  ", ");
+		}
+	}
 }
