725573f988f5b74cb8e1579bcb0640681277c38a
Gereon Recht <gereon.recht@protonmail.com>
2018-01-30 22:12:20 +0100

    Added tests for MinimalTotalColoring

--- src/test/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/color/MinimalTotalColoringTest.java
+++ src/test/java/edu/kit/ipd/dbis/org/jgrapht/additions/alg/color/MinimalTotalColoringTest.java
@@ -26,6 +26,37 @@ public class MinimalTotalColoringTest {
 
 	@Test
 	public void getColoring2() {
+		PropertyGraph graph = new PropertyGraph();
+		graph.addVertex("a");
+		graph.addVertex("b");
+		graph.addVertex("c");
 
+		graph.addEdge("a", "b");
+		graph.addEdge("a", "c");
+		graph.addEdge("b", "c");
+
+		MinimalTotalColoring totalColoring = new MinimalTotalColoring(graph);
+		TotalColoringAlgorithm.TotalColoring coloring = totalColoring.getColoring();
+		assertEquals(3, coloring.getNumberColors());
+	}
+
+	@Test
+	public void getColoring3() {
+		PropertyGraph graph = new PropertyGraph();
+		graph.addVertex("a");
+		graph.addVertex("b");
+		graph.addVertex("c");
+		graph.addVertex("d");
+
+		graph.addEdge("a", "b");
+		graph.addEdge("a", "c");
+		graph.addEdge("a", "d");
+		graph.addEdge("b", "c");
+		graph.addEdge("b", "d");
+		graph.addEdge("c", "d");
+
+		MinimalTotalColoring totalColoring = new MinimalTotalColoring(graph);
+		TotalColoringAlgorithm.TotalColoring coloring = totalColoring.getColoring();
+		assertEquals(4, coloring.getNumberColors());
 	}
 }
