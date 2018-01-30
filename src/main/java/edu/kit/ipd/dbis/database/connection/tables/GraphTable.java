c3acd897aff51bab208f474ba3377dc60150e939
Cem Özcan <uvtwy@student.kit.edu>
2018-01-30 23:18:54 +0100

    Class "GraphTable" : Invalid characters removed from bfsCode

--- src/main/java/edu/kit/ipd/dbis/database/connection/tables/GraphTable.java
+++ src/main/java/edu/kit/ipd/dbis/database/connection/tables/GraphTable.java
@@ -275,7 +275,7 @@ public class GraphTable extends Table {
 		int[] bfsCode = code.getCode();
 
 		for (int i = 0; i < bfsCode.length; i++) {
-			s += (i != bfsCode.length - 1) ? (bfsCode[i] + "|") : (bfsCode[i]);
+			s += (i != bfsCode.length - 1) ? (bfsCode[i] + "a") : (bfsCode[i]);
 		}
 		return s;
 
