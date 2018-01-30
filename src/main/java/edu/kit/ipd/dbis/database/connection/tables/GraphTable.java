2fc0ae33e14c87cd5d7f2d5cabd695c6a1915bb5
Cem Özcan <uvtwy@student.kit.edu>
2018-01-30 23:50:13 +0100

    Class "GraphTable" : BfsCode can now be stored in the Database.

--- src/main/java/edu/kit/ipd/dbis/database/connection/tables/GraphTable.java
+++ src/main/java/edu/kit/ipd/dbis/database/connection/tables/GraphTable.java
@@ -275,7 +275,7 @@ public class GraphTable extends Table {
 		int[] bfsCode = code.getCode();
 
 		for (int i = 0; i < bfsCode.length; i++) {
-			s += (i != bfsCode.length - 1) ? (bfsCode[i] + "a") : (bfsCode[i]);
+			s += (i != bfsCode.length - 1) ? (bfsCode[i] + ";") : (bfsCode[i]);
 		}
 		return s;
 
