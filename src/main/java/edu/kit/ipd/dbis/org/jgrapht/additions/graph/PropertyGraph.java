0143b85d4ef9e8e1d93bddeb126d1b4e8c79cf84
Felix <koehlerfelix@arcor.de>
2018-01-30 20:44:37 +0100

    remove clone method for propertyGraph

--- src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/graph/PropertyGraph.java
+++ src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/graph/PropertyGraph.java
@@ -142,13 +142,4 @@ public class PropertyGraph<V, E> extends SimpleGraph {
 		}
 		return matrix;
 	}
-
-	/**
-	 * clones this graph
-	 * @return a clone of this graph
-	 */
-	public PropertyGraph clone() {
-		//TODO: implement me
-		return null;
-	}
 }
