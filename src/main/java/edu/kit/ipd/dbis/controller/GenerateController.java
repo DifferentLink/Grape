a87feaddafe50618434c7cebf8014651344a16bd
Cem Özcan <uvtwy@student.kit.edu>
2018-01-31 19:36:47 +0100

    Class "Table" : Field "connection" added to allow unlimited access to the database.

--- src/main/java/edu/kit/ipd/dbis/controller/GenerateController.java
+++ src/main/java/edu/kit/ipd/dbis/controller/GenerateController.java
@@ -139,7 +139,6 @@ public class GenerateController {
 				database.addGraph(graph);
 			} catch (DatabaseDoesNotExistException | TablesNotAsExpectedException | AccessDeniedForUserException
 					| ConnectionFailedException | InsertionFailedException | UnexpectedObjectException e) {
-				e.printStackTrace();
 				log.addEvent(new Event(MESSAGE, e.getMessage(), Collections.EMPTY_SET));
 			}
 		}
