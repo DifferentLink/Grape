0f7adf32b83aecfb49f09f37d0a02dfc204e8409
Gereon Recht <gereon.recht@protonmail.com>
2018-02-02 21:29:24 +0100

    Implemented TotalColoring and VertexColoring properties

--- src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/graph/properties/complex/VertexColoring.java
+++ src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/graph/properties/complex/VertexColoring.java
@@ -3,10 +3,10 @@ package edu.kit.ipd.dbis.org.jgrapht.additions.graph.properties.complex;
 import edu.kit.ipd.dbis.org.jgrapht.additions.alg.color.MinimalVertexColoring;
 import edu.kit.ipd.dbis.org.jgrapht.additions.graph.PropertyGraph;
 import edu.kit.ipd.dbis.org.jgrapht.additions.graph.properties.ComplexProperty;
-
 import org.jgrapht.alg.interfaces.VertexColoringAlgorithm;
 import org.kohsuke.MetaInfServices;
 
+import java.util.List;
 
 
 /**
@@ -24,8 +24,8 @@ public class VertexColoring extends ComplexProperty {
 	}
 
 	@Override
-	protected VertexColoringAlgorithm.Coloring calculateAlgorithm(PropertyGraph graph) {
+	protected List<VertexColoringAlgorithm.Coloring> calculateAlgorithm(PropertyGraph graph) {
 		MinimalVertexColoring alg = new MinimalVertexColoring(graph);
-		return alg.getColoring();
+		return alg.getAllColorings();
 	}
 }
