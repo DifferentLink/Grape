77df82911ff9af7588afea56ceadc81bff2914fd
Gereon Recht <gereon.recht@protonmail.com>
2018-02-04 18:33:06 +0100

    Implemented property LargestCliqueSize

---
+++ src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/graph/properties/integer/LargestCliqueSize.java
@@ -0,0 +1,30 @@
+package edu.kit.ipd.dbis.org.jgrapht.additions.graph.properties.integer;
+
+import edu.kit.ipd.dbis.org.jgrapht.additions.graph.PropertyGraph;
+import edu.kit.ipd.dbis.org.jgrapht.additions.graph.properties.IntegerProperty;
+import edu.kit.ipd.dbis.org.jgrapht.additions.graph.properties.complex.Cliques;
+
+import java.util.List;
+import java.util.Set;
+
+public class LargestCliqueSize extends IntegerProperty {
+	/**
+	 * Standard constructor
+	 *
+	 * @param graph input graph
+	 */
+	public LargestCliqueSize(PropertyGraph graph) {
+		super(graph);
+	}
+
+	@Override
+	protected Integer calculateAlgorithm(PropertyGraph graph) {
+		int largestCliqueSize = 0;
+		for (Set<Object> s : ((List<Set<Object>>) graph.getProperty(Cliques.class).getValue())) {
+			if (s.size() > largestCliqueSize) {
+				largestCliqueSize = s.size();
+			}
+		}
+		return largestCliqueSize;
+	}
+}
