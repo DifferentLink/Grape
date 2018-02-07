1858f530bea02a638394c0a536b7fbe95103d047
Gereon Recht <gereon.recht@protonmail.com>
2018-02-07 17:59:25 +0100

    Changed test to compute actual total coloring

--- src/test/java/edu/kit/ipd/dbis/gui/grapheditor/RenderableGraphTest.java
+++ src/test/java/edu/kit/ipd/dbis/gui/grapheditor/RenderableGraphTest.java
@@ -1,12 +1,9 @@
 package edu.kit.ipd.dbis.gui.grapheditor;
 
+import edu.kit.ipd.dbis.org.jgrapht.additions.alg.color.MinimalTotalColoring;
 import edu.kit.ipd.dbis.org.jgrapht.additions.alg.interfaces.TotalColoringAlgorithm;
 import edu.kit.ipd.dbis.org.jgrapht.additions.graph.PropertyGraph;
-import org.jgrapht.alg.interfaces.VertexColoringAlgorithm;
 import org.junit.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -29,31 +26,6 @@ public class RenderableGraphTest {
 	}
 
 	@Test
-	public void constructorTestVertexColoring() {
-		PropertyGraph graph = new PropertyGraph();
-		graph.addVertex("a");
-		graph.addVertex("b");
-		graph.addVertex("c");
-		graph.addVertex("d");
-		graph.addEdge("a", "b");
-		graph.addEdge("a", "c");
-		graph.addEdge("b", "c");
-		graph.addEdge("b", "d");
-
-		Map<String, Integer> colors = new HashMap<>();
-		colors.put("a", 0);
-		colors.put("b", 1);
-		colors.put("c", 2);
-		colors.put("d", 0);
-		VertexColoringAlgorithm.Coloring coloring = new VertexColoringAlgorithm.ColoringImpl(colors, 3);
-
-		RenderableGraph renderableGraph = new RenderableGraph(graph, coloring);
-
-		assertEquals(graph.vertexSet().size(), renderableGraph.getVertices().size());
-		assertEquals(graph.edgeSet().size(), renderableGraph.getEdges().size());
-	}
-
-	@Test
 	public void constructorTestTotalColoring() {
 		PropertyGraph<Integer, Integer> graph = new PropertyGraph();
 		graph.addVertex(1);
@@ -65,19 +37,9 @@ public class RenderableGraphTest {
 		graph.addEdge(2, 3);
 		graph.addEdge(2, 4);
 
-		Map<Integer, Integer> vertexColors = new HashMap<>();
-		vertexColors.put(1, 0);
-		vertexColors.put(2, 1);
-		vertexColors.put(3, 2);
-		vertexColors.put(4, 0);
-		Map<Integer, Integer> edgeColors = new HashMap<>();
-		edgeColors.put(0, 0);
-		edgeColors.put(1, 1);
-		edgeColors.put(2, 2);
-		edgeColors.put(3, 3);
+		MinimalTotalColoring alg = new MinimalTotalColoring(graph);
 
-		TotalColoringAlgorithm.TotalColoring coloring = new TotalColoringAlgorithm.TotalColoringImpl(vertexColors, edgeColors, 4);
-
+		TotalColoringAlgorithm.TotalColoring coloring = alg.getColoring();
 		RenderableGraph renderableGraph = new RenderableGraph(graph, coloring);
 
 		assertEquals(graph.vertexSet().size(), renderableGraph.getVertices().size());
