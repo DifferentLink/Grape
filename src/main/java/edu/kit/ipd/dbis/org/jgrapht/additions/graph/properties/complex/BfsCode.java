16aa958a1bf219eb57a6056325b24a35a62dddae
Collin Lorbeer <collinlorbeer@freenet.de>
2018-02-11 12:56:52 +0100

    Squashed commit of the following:
    commit fb9390e2f11527ad74463e3e89a600a460e84e34
    Merge: 04a29ae5 63055aca
    Author: Gereon Recht <gereon.recht@protonmail.com>
    Date:   Sun Feb 11 12:46:25 2018 +0100
    
        Merge pull request #187 from DifferentLink/remove-serviceloader
    
        Remove metainf-services dependency
    
    commit 63055acad0f45b73cc220e056a91a381273d6154
    Author: Gereon Recht <gereon.recht@protonmail.com>
    Date:   Sun Feb 11 12:43:04 2018 +0100
    
        Remove metainf-services dependency

--- src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/graph/properties/complex/BfsCode.java
+++ src/main/java/edu/kit/ipd/dbis/org/jgrapht/additions/graph/properties/complex/BfsCode.java
@@ -2,15 +2,12 @@ package edu.kit.ipd.dbis.org.jgrapht.additions.graph.properties.complex;
 
 import edu.kit.ipd.dbis.org.jgrapht.additions.alg.interfaces.BfsCodeAlgorithm;
 import edu.kit.ipd.dbis.org.jgrapht.additions.alg.interfaces.ProfileDensityAlgorithm;
-import edu.kit.ipd.dbis.org.jgrapht.additions.graph.Property;
 import edu.kit.ipd.dbis.org.jgrapht.additions.graph.PropertyGraph;
 import edu.kit.ipd.dbis.org.jgrapht.additions.graph.properties.ComplexProperty;
-import org.kohsuke.MetaInfServices;
 
 /**
  * the bfs code property
  */
-@MetaInfServices(Property.class)
 public class BfsCode extends ComplexProperty {
 	/**
 	 * Standard constructor
